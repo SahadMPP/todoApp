bc91626d4078a5f8d05c4fe091db1e2c0865db32
Sahad Mp <muhdsahad4916@gmail.com>
2023-11-07 19:57:25 +0530

    try

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,9 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('hello'),
-      ),
+      appBar: AppBar(),
     );
   }
 }
