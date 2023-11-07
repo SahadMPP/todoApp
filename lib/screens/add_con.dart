ef741ae32a8dfc6d22aee62ae489cabfe5713fcd
Sahad Mp <muhdsahad4916@gmail.com>
2023-11-08 01:09:13 +0530

    done add and delete

--- lib/screens/add_con.dart
+++ lib/screens/add_con.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ScondScreen extends StatefulWidget {
   const ScondScreen({super.key});
@@ -8,6 +11,8 @@ class ScondScreen extends StatefulWidget {
 }
 
 class _ScondScreenState extends State<ScondScreen> {
+  TextEditingController titleEditingController = TextEditingController();
+  TextEditingController dicriptionEditingContorller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,25 +25,69 @@ class _ScondScreenState extends State<ScondScreen> {
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
-          const TextField(
-            decoration: InputDecoration(hintText: 'Text'),
+          TextField(
+            controller: titleEditingController,
+            decoration: const InputDecoration(hintText: 'Title'),
           ),
           const SizedBox(height: 20),
-          const TextField(
-            decoration: InputDecoration(hintText: 'Discription'),
+          TextField(
+            controller: dicriptionEditingContorller,
+            decoration: const InputDecoration(hintText: 'Discription'),
             maxLines: 5,
             keyboardType: TextInputType.multiline,
           ),
           const SizedBox(height: 20),
-          ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+          ElevatedButton(onPressed: submitData, child: const Text('Submit'))
         ],
       ),
     );
   }
 
-  void submitData() {
+  void submitData() async {
     // get the data from form
+    final title = titleEditingController.text;
+    final discription = dicriptionEditingContorller.text;
+    final body = {
+      "title": title,
+      "description": discription,
+      "is_completed": false
+    };
     // submit data to the server
+    const url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final responce = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
     // show success of failer based on status
+    if (responce.statusCode == 201) {
+      titleEditingController.text = '';
+      dicriptionEditingContorller.text = '';
+      showScatforldMassegTrue('Creation succuess');
+    } else {
+      showScatforldMassegFalse('Creation failer');
+      // ignore: avoid_print
+      print(responce.body);
+    }
+  }
+
+  showScatforldMassegTrue(String massege) {
+    final snackBar = SnackBar(
+      content: Text(massege),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  showScatforldMassegFalse(String massege) {
+    final snackBar = SnackBar(
+      content: Text(
+        massege,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_con.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,6 +12,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List items = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    getTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,13 +34,101 @@ class _HomeState extends State<Home> {
           textAlign: TextAlign.center,
         ),
       ),
+      body: Visibility(
+        visible: isLoading,
+        replacement: RefreshIndicator(
+          onRefresh: getTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              final id = item["_id"] as String;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}')),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+                trailing: PopupMenuButton(
+                  onSelected: (value) {
+                    if (value == 'edit') {
+                      // go to edit page
+                    }
+                    if (value == 'delete') {
+                      // delete the item
+                      deleteById(id);
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return [
+                      const PopupMenuItem(
+                        value: 'edit',
+                        child: Text('Edit'),
+                      ),
+                      const PopupMenuItem(
+                        value: 'delete',
+                        child: Text('Delete'),
+                      ),
+                    ];
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+        child: const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 
-  navigatorToAddscreen() {
+  Future<void> navigatorToAddscreen() async {
     final route = MaterialPageRoute(
       builder: (context) => const ScondScreen(),
     );
-    Navigator.of(context).push(route);
+
+    await Navigator.of(context).push(route);
+    setState(() {
+      isLoading = true;
+    });
+    getTodo();
+  }
+
+  Future<void> getTodo() async {
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final respons = await http.get(uri);
+    if (respons.statusCode == 200) {
+      final json = jsonDecode(respons.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> deleteById(id) async {
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+    if (response.statusCode == 200) {
+      final item = items.where((element) => (element)['_id'] != id).toList();
+      setState(() {
+        items = item;
+      });
+    } else {
+      showScatforldMassegFalse('Delete is Field');
+    }
+  }
+
+  showScatforldMassegFalse(String massege) {
+    final snackBar = SnackBar(
+      content: Text(
+        massege,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
